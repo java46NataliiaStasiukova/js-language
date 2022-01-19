9f1f8769fe8cf693d80164ad2bbe2a593b8dcca9
Nataliia <nstasiukova@gmail.com>
2022-01-19 20:03:11 +0200

    Homework-12

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
-
- // Homework-11
- let code = "0123456789abcdef";
- let number = 56;
- checkCode(code);
-function checkCode(code){
-    for(i = 0; i < code.length; i++){
-        for(j = i + 1; j < code.length; j++){
-        if(`${code[i]}` == `${code[j]}`){
-            console.log("Error!");
-            return;
-            }
-        }
+//Homework-12
+//1
+function sortOddsEven(arr){
+    let odds = [];
+    let even = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 == 0) even.push(arr[i]);
+        else odds.push(arr[i]);
     }
-    console.log(encode(number,code));
+    even.sort((a, b) => a - b);
+    odds.sort((a,b) => b - a);
+    even.push(...odds);
+    return even;
 }
-function encode (num, codingString){
-    let res = "";
-    do{
-        const digit = Math.trunc(num % codingString.length);
-        const symb = getSymbol(digit);
-        res = symb + res;
-          num = Math.trunc(num / codingString.length);
-    }while(num >= 1);
-    return res;
+const arflw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+console.log("ex.1")
+console.log(sortOddsEven(arflw));
+//2 version-1
+function matrixTransp(matrix){
+    let arrI = [];
+    for(let i = 0; i < matrix.length; i++){
+       let arrJ = [];
+        for (let j = 0; j < matrix.length; j++) {
+           arrJ.push(matrix[j][i]);
+        }
+        arrJ.filter(element => element != null);
+        arrJ = arrJ.filter(Boolean);
+        arrI.push(arrJ);
+    }
+    matrixPrint(arrI);
 }
-function getSymbol(digit){
-    return "" +`${code[digit]}`;
+function matrixPrint(matrix){
+    for(let i = 0; i < matrix.length; i++){
+        let row = '';
+        for (let j = 0; j < matrix[i].length; j++){
+           row = row + matrix[i][j] + '  ';
+        }
+        console.log(row);
+     }
 }
+//2 version-2
+function matrixTransp1(matrix){
+    for(let i = 0; i < matrix.length; i++){
+        let row = '';
+        for (let j = 0; j < matrix.length; j++){
+           row = row + matrix[j][i] + '  ';
+        }
+        console.log(row);
+     }
+}
+// Test:
+const matrix1 = [[1, 2], [3, 4], [5, 6]];
+const matrix2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log("ex.2-version-1:")
+matrixTransp(matrix1);
+matrixTransp(matrix2);
+console.log("ex.2-version-2:")
+console.log(matrixTransp1(matrix1));
+console.log(matrixTransp1(matrix2));
+
+
+
+
