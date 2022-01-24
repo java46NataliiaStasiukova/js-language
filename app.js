54c172c829897ba42c9cd022783d70143868ed9b
Nataliia <nstasiukova@gmail.com>
2022-01-24 06:28:58 +0200

    Homework-13

--- app.js
+++ app.js
@@ -1,59 +1,37 @@
-//Homework-12
-//1
-const comparator = function sortOddsEven(a,b){
-    if(a % 2 == 0 && b % 2 == 0) return a - b;
-    if (a % 2 != 0 && b % 2 == 0) return 1;
-    if (a % 2 == 0 && b % 2 != 0) return -1;
-    else return b - a;
+//Homework-13
+//1. myForEach
+function myForEach(arr,func){
+    for(i = 0; i < arr.length; i++){
+        func(arr[i], i, arr)
+    }
 }
-const comparator2 = (a,b) => a%2 ? (b%2 ? b - a : 1) : (b%2 ? -1 : a - b);
-//oposite:
-const comparator3 = (a,b) => a%2 ? (b%2 ? a - b : -1) : (b%2 ? 1 : b - a);
+function callback(n, i, ar){
+    console.log(`array[${i}] = ${n}`);
+}
+array = [3,4,5,6,7,8];
+myForEach(array,callback);
+// or another callback for myForEach:
+let string = "";
+myForEach(array, n => string += n + "#");
+console.log(string);
+//2. myMap
+function myMap(arr,func){
+    myForEach(arr,func);
+}
+let arr2 = [];
+myMap(array, n => arr2.push(n * 2));
+console.log(arr2);
+
+
+
+
+
+
 
 
     
 
-const arflw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
-console.log(arflw.sort(comparator));
-console.log(arflw.sort(comparator2));
-console.log(arflw.sort(comparator3));
 
-//2 version-1
-//[[1, 2, 3], [4, 5, 6]];
-function matrixTransp(matrix){
-   const res = [];
-   for(let i = 0; i < matrix[0].length; i++){
-    res.push([]);
-    for(let j = 0; j < matrix.length; j++){
-        res[i].push(matrix[j][i]);
-    }
-   }
-   return res;
-}
-function displayMatrix(arr) {
-    for(let i = 0; i < arr.length; i++){
-        let row = '';
-        for(let j = 0; j < arr[i].length; j++){
-            row = row + arr[i][j] + '  ';
-        }
-        console.log(row);
-    }
-    console.log('');
-    
-}
-// Test:
-const matrix1 = [[1, 2], [3, 4], [5, 6]];
-const matrix2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-const matrix3 = [[1, 2, 3], [4, 5, 6]];
-//console.log("ex.2-version-1:")
-//matrixTransp(matrix1);
-//matrixTransp(matrix2);
-//matrixTransp(matrix3);
-//console.log("ex.2-version-2:")
-displayMatrix(matrixTransp(matrix1));
-//displayMatrix(matrix1);
-displayMatrix(matrixTransp(matrix2));
-displayMatrix(matrixTransp(matrix3));
 
 
 
